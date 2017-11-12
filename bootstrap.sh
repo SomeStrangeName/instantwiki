5dd15f0952106d52098b50a4aaa54d6ef2485368
SomeStrangeName <SomeStrangeName@users.noreply.github.com>
2017-11-12 12:52:18 +0100

    Some comments in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -42,8 +42,14 @@ sudo mkdir -p /data/mysql
 #sudo chown root.www-data /data/wiki/images/* -R
 
 # should be replaced with a docker compose file
+# mariadb container
 sudo docker run --name mysql -e MYSQL_ROOT_PASSWORD=${dbpw} -v /data/mysql:/var/lib/mysql -p 3306:3306 -d mariadb:latest
+# mediawiki container (initial startup)
 sudo docker run --name wiki --link mysql:mysql -p 8080:80 -v /data/wiki/images:/var/www/html/images -d mediawiki:latest
+# if you place the LocalSettings.php file use following startup
+#sudo docker run --name wiki --link mysql:mysql -p 8080:80 -v /data/wiki/images:/var/www/html/images -v /data/wiki/LocalSettings.php:/var/www/html/LocalSettings.php -d mediawiki:latest
+# this will create an initial empty database for the mediawiki installation
 sudo mysql -h 127.0.0.1 -u ${dbuser} -p${dbpw} < /vagrant/wiki_init/createdb.sql
-sudo gunzip < /vagrant/wiki_backup/wiki.sql.gz | mysql -h 127.0.0.1 -u ${dbuser} -p${dbpw} mywiki
+# this will restore a mediawiki SQL dump
+# sudo gunzip < /vagrant/wiki_backup/wiki.sql.gz | mysql -h 127.0.0.1 -u ${dbuser} -p${dbpw} mywiki
 
